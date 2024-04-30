0ea6be167d56f99bf0fd4953f73224336727c782
ghassenjebari <ghassenjebari1@gmail.com>
2024-04-30 19:07:34 +0100

    crud publication fonctionelle

--- src/main/java/com/fogbank/springsecurity/services/forum/IPublicationInitialeService.java
+++ src/main/java/com/fogbank/springsecurity/services/forum/IPublicationInitialeService.java
@@ -2,6 +2,8 @@ package com.fogbank.springsecurity.services.forum;
 
 import com.fogbank.springsecurity.entities.forum.PublicationInitiale;
 
-public interface IPublicationInitialeService extends ICrudService<PublicationInitiale> {
+import java.util.Optional;
 
+public interface IPublicationInitialeService extends ICrudService<PublicationInitiale> {
+    public Optional<PublicationInitiale> afficherDetails(int id);
 }
