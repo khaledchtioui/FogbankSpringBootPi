e9ba65526e2f3b096ff1b14023c38437eab1f16e
ghassenjebari <ghassenjebari1@gmail.com>
2024-05-07 15:11:50 +0100

    like et SignalementPost fonctionelle

--- src/main/java/com/fogbank/springsecurity/services/forum/implServiceForum/SmsTool.java
+++ src/main/java/com/fogbank/springsecurity/services/forum/implServiceForum/SmsTool.java
@@ -9,17 +9,6 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class SmsTool {
-    // Find your Account Sid and Token at twilio.com/user/account
-    public static final String ACCOUNT_SID = "AC39fa940bdd1be4d3294e6cb4f449bbba";
-    public static final String AUTH_TOKEN = "c92414983dd4f9e8693dfd02e36c9ba8";
 
-    public static void envoyer(String msg,String numDest) {
-        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-
-        Message message = Message.creator(new PhoneNumber(numDest),
-                new PhoneNumber("+15616009250"),
-                msg).create();
-
-        System.out.println(message.getSid());
     }
 }
